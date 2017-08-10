6ccd25bdd178dcd599a84c190ac821184eb16da2
tzurvaza <tzurvaza@gmail.com>
2017-08-10 11:49:39 +0300

    test2

--- src/main/java/il/co/electriccollege/collections/CollectionsPlayground.java
+++ src/main/java/il/co/electriccollege/collections/CollectionsPlayground.java
@@ -8,6 +8,7 @@ import java.util.*;
 public class CollectionsPlayground {
 
     public static void main(String[] args) {
+        //new branch
         System.out.println("Start list collection !!!");
         ArrayList<String> stringList = new ArrayList<String>();
         stringList.add("hello");
