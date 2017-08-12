fee44a2a710185cff0436142123c4d2d3a463e67
borka-PC\borka <boris.goman@gmail.com>
2017-08-12 17:41:48 +0300

    Revert "del for new and change"
    This reverts commit 10457397c5f5fce268bf63e78263df290cf9bebc.

--- src/main/java/il/co/electriccollege/collections/CollectionsPlayground.java
+++ src/main/java/il/co/electriccollege/collections/CollectionsPlayground.java
@@ -1,75 +1,66 @@
 package il.co.electriccollege.collections;
 
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-
+/**
+ * Created by yaakov on 8/1/17.
+ */
 
 public class CollectionsPlayground {
 
-
     public static void main(String[] args) {
-        System.out.println("Start list collection");
-        ArrayList<ArrayList> mainList = new ArrayList<>();
-//        stringList.add("hello");
-//        stringList.add("world");
-//
-//        // the values of the list are accessible by index
-//        System.out.println("List member 0: " + stringList.get(0));
-//        System.out.println("List member 1: " + stringList.get(1));
-//
-//        // this is how we iterate over the whole list
-        ArrayList<String> list1= new ArrayList<>();
-        list1.add("hello1");
-        list1.add("world1");
-        ArrayList<String> list2= new ArrayList<>();
-        list2.add("hello2");
-        list2.add("world2");
-        ArrayList<String> list3= new ArrayList<>();
-        list3.add("hello3");
-        list3.add("world3");
+        //b1
+        System.out.println("Start list collection !!!");
+        ArrayList<String> stringList = new ArrayList<String>();
+        stringList.add("hello");
+        stringList.add("world");
+
+        // the values of the list are accessible by index
+        System.out.println("List member 0: " + stringList.get(0));
+        System.out.println("List member 1: " + stringList.get(1));
+
+        // this is how we iterate over the whole list
+        for(String s: stringList){
+            System.out.println(s);
 
 
-        for(ArrayList s: mainList){
-            System.out.println("External list element: "+ s.toString());
-            ArrayList <String>  tempList = s;
-            System.out.println("External list element: "+ tempList.toString());
-
-            for (String curr: tempList ) {
-
-                System.out.println("Inner element: "+ curr.toString());
-            }
-            System.out.println("");
         }
-        // ------------------------------------------------------------------------
-//        System.out.println("");
-//        System.out.println("Start set collection");
-//        HashSet<String> stringSet = new HashSet<String>();
-//        stringSet.add("electric");
-//        stringSet.add("college");
-//
-//        // set members are not accessible individually, only by iterating through the set
-//        for(String s: stringSet){
-//            System.out.println("Set member: " + s);
-//        }
-//
-//     // ----------------------------------------------
-//        System.out.println("");
-//        System.out.println("Start map collection");
-//        HashMap<String, String> stringMap = new HashMap<String, String>();
-//        stringMap.put("city", "Hadera");
-//        stringMap.put("street", "HaShalom");
-//
-//        // map members are accessible by key
-//        System.out.println("key: city; value: " + stringMap.get("city"));
-//        System.out.println("key: street; value: " + stringMap.get("street"));
-//
-//        // iteration over a map is possible by getting the entry set
-//        for(Map.Entry<String, String> entry: stringMap.entrySet()){
-//            System.out.println(entry);
-//        }
+
+        System.out.println("");
+        System.out.println("Start set collection");
+        HashSet<String> stringSet = new HashSet<String>();
+        stringSet.add("electric");
+        stringSet.add("college");
+
+        stringSet.add("daniel");
+        stringSet.add("commit in master");
+
+=======
+        stringSet.add("collegess");
+        stringSet.add("college");
+        stringSet.add("college");
+
+
+        // set members are not accessible individually, only by iterating through the set
+        for(String s: stringSet){
+            System.out.println("Set member: " + s);
+        }
+
+        System.out.println("");
+        System.out.println("Start map collection");
+        HashMap<String, String> stringMap = new HashMap<String, String>();
+        stringMap.put("city", "Hadera");
+        stringMap.put("street", "HaShalom");
+        stringMap.put("street2", "HaShalom");
+        // map members are accessible by key
+        String news = "";
+        System.out.println("key: city; value: " + stringMap.get("city"));
+        System.out.println("key: street; value: " + stringMap.get("street"));
+
+        // iteration over a map is possible by getting the entry set
+        for(Map.Entry<String, String> entry: stringMap.entrySet()){
+            System.out.println(entry);
+        }
 
     }
 }
