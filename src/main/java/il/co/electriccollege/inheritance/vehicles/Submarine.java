14bdeb99b9e8d85a8a72e03e520db8ba3f379b24
Yaakov Gesher <yaakov@gesher.net>
2017-08-14 11:02:25 +0300

    added answers to inheritance drill

---
+++ src/main/java/il/co/electriccollege/inheritance/vehicles/Submarine.java
@@ -0,0 +1,28 @@
+package il.co.electriccollege.inheritance.vehicles;
+
+import il.co.electriccollege.inheritance.faces.Submergeable;
+
+/**
+ * Created by yaakov on 8/14/17.
+ */
+public class Submarine extends Vehicle implements Submergeable {
+
+    public Submarine(int speed) {
+        super(speed);
+    }
+
+    @Override
+    public boolean isUnderwater() {
+        return this.speed > 20;
+    }
+
+    @Override
+    public int getMaxTimeUnderwater() {
+        return 24 * 60;
+    }
+
+    @Override
+    public String getMedium() {
+        return "water";
+    }
+}
