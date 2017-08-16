a52cbe59fffe5053f1f150b9b5601155696c665d
Yaakov Gesher <yaakov@gesher.net>
2017-08-16 23:50:35 +0300

    implemented part of the exercise

---
+++ restaurant_exercises/src/main/java/il/co/electriccollege/restaurant/dishes/AbstractDish.java
@@ -0,0 +1,58 @@
+package il.co.electriccollege.restaurant.dishes;
+
+/**
+ * Created by yaakov on 8/16/17.
+ */
+public abstract class AbstractDish {
+    private String name;
+    private int id;
+    private String description;
+    private float price;
+
+    public AbstractDish(String name, float price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    protected void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    protected void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getPreparationTime() {
+        return preparationTime;
+    }
+
+    public void setPreparationTime(int preparationTime) {
+        this.preparationTime = preparationTime;
+    }
+
+    private int preparationTime;
+}
