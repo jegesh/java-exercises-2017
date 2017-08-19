ddbf4db672e660e6daad15584939b5f43f5e81f8
bahaluly <bahaluly@gmail.com>
2017-08-19 13:15:09 +0300

    my first changes in Order and Priority

--- restaurant_exercises/src/main/java/il/co/electriccollege/restaurant/orders/Order.java
+++ restaurant_exercises/src/main/java/il/co/electriccollege/restaurant/orders/Order.java
@@ -4,4 +4,32 @@ package il.co.electriccollege.restaurant.orders;
  * Created by yaakov on 8/16/17.
  */
 public class Order {
+
+    private Date orderTime;
+    private ArrayList<int> orderDishes;
+
+    public Order() {
+    }
+
+    public Order(Date orderTime, ArrayList<int> orderDishes) {
+        this.orderTime = orderTime;
+        this.orderDishes = orderDishes;
+    }
+
+    public Date getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(Date orderTime) {
+        this.orderTime = orderTime;
+    }
+
+    public void getTotal() {
+
+            float total = 0;
+            for(int i: orderDishes){
+                total += getPrice;
+            }
+
+    }
 }
--- restaurant_exercises/src/main/java/il/co/electriccollege/restaurant/orders/Priority.java
+++ restaurant_exercises/src/main/java/il/co/electriccollege/restaurant/orders/Priority.java
@@ -4,4 +4,5 @@ package il.co.electriccollege.restaurant.orders;
  * Created by yaakov on 8/16/17.
  */
 public enum Priority {
+    HIGH, REGULAR, LOW
 }
