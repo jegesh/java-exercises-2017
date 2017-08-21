60c5b3ca97fec8092810a8e329a88f1127143aef
bahaluly <bahaluly@gmail.com>
2017-08-21 11:25:12 +0300

    first files

---
+++ library/src/main/java/il/co/electriccollege/library/media/AbstractMedia.java
@@ -0,0 +1,13 @@
+package il.co.electriccollege.library.media;
+
+import java.util.Date;
+
+public class AbstractMedia {
+
+    private String name;
+    private int id;
+    private MediaStatus status;
+    private String publisher;
+    private Date checkOutDate;
+    private Date publicationDate;
+}
---
+++ library/src/main/java/il/co/electriccollege/library/media/MediaStatus.java
@@ -0,0 +1,7 @@
+package il.co.electriccollege.library.media;
+
+public enum MediaStatus {
+
+    LOANED,
+    AVAILABLE;
+}
