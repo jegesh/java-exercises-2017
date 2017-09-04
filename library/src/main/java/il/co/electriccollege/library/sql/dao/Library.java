fff9b4922036cff266d2acc763b3933b4ff2f72f
DESKTOP-BO5RVI6\Comp14 <boris.goman@gmail.com>
2017-09-04 11:40:39 +0300

    close borka

---
+++ library/src/main/java/il/co/electriccollege/library/sql/dao/Library.java
@@ -0,0 +1,44 @@
+package il.co.electriccollege.library.sql.dao;
+
+/**
+ * Created by Comp14 on 04/09/2017.
+ */
+public class Library {
+
+private  int id;
+private String name;
+private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
