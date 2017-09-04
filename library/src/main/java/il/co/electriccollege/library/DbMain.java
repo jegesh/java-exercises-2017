d5caa572d5622fcc950a4fde671540b77d7e7c29
Yaakov Gesher <yaakov@gesher.net>
2017-09-04 11:28:08 +0300

    added library dal

--- library/src/main/java/il/co/electriccollege/library/DbMain.java
+++ library/src/main/java/il/co/electriccollege/library/DbMain.java
@@ -2,10 +2,12 @@ package il.co.electriccollege.library;
 
 import il.co.electriccollege.library.sql.DatabaseConnector;
 import il.co.electriccollege.library.sql.dal.MediaDal;
+import il.co.electriccollege.library.sql.dao.AbstractMedia;
 import il.co.electriccollege.library.sql.dao.Book;
 import il.co.electriccollege.library.sql.dao.MediaType;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by yaakov on 8/23/17.
@@ -16,14 +18,18 @@ public class DbMain {
         String configFile = args[0];
 
         // retrieval
-        MediaDal mediaDal = new MediaDal(new DatabaseConnector(configFile));
+        MediaDal mediaDal = new MediaDal(new DatabaseConnector());
 //        AbstractMedia returnedMedia = mediaDal.getById(2);
 //        System.out.println(returnedMedia);
 
         // Add to library
-        Book b = new Book("The Dibbuk", "Random House", Main.getDateWithCalendar("09/09/1980"));
-        b.setType(MediaType.BOOK);
-        boolean added = mediaDal.addMediaToLibrary(b);
-        System.out.println("Was book added to library? " + added);
+//        Book b = new Book("The Dibbuk", "Random House", Main.getDateWithCalendar("09/09/1980"));
+//        b.setType(MediaType.BOOK);
+//        boolean added = mediaDal.addMediaToLibrary(b);
+//        System.out.println("Was book added to library? " + added);
+
+        List<AbstractMedia> media = mediaDal.getByTypeWithLibrary(MediaType.BOOK);
+        System.out.println(media);
+        System.out.println(media.get(0).getLibrary());
     }
 }
