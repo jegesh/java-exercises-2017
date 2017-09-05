76baf5e45dafc8029eb4003e56ced510ccbeda15
Irena Khachaturyan <irail@bk.ru>
2017-09-05 09:18:12 +0300

    pulled from master

--- restaurant_exercises/src/main/java/il/co/electriccollege/restaurant/orders/Order.java
+++ restaurant_exercises/src/main/java/il/co/electriccollege/restaurant/orders/Order.java
@@ -1,6 +1,6 @@
 package il.co.electriccollege.restaurant.orders;
 
-import com.sun.tools.corba.se.idl.constExpr.Or;
+//import com.sun.tools.corba.se.idl.constExpr.Or;
 import il.co.electriccollege.restaurant.dishes.AbstractDish;
 
 import java.util.ArrayList;
