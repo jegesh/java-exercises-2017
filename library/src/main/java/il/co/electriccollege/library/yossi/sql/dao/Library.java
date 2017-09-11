b7a79a7a91937adaae91681b4e0efc02b06be30e
bahaluly <bahaluly@gmail.com>
2017-09-11 08:59:07 +0300

    change #C2 home changes

--- library/src/main/java/il/co/electriccollege/library/yossi/sql/dao/Library.java
+++ library/src/main/java/il/co/electriccollege/library/yossi/sql/dao/Library.java
@@ -1,4 +1,38 @@
 package il.co.electriccollege.library.yossi.sql.dao;
 
 public class Library {
+
+    private int id;
+    private String name;
+    private String address;
+
+    public Library(int id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
